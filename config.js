dbdc0da3078e5d540f2ada86923172ac154c92a1
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-11-03 15:37:50 +0100

    finished practice

---
+++ config.js
@@ -0,0 +1,13 @@
+const colors = require('taiwindcss/colors');
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                fuchia: colors.fuchsia,
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
